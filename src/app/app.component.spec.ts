7a086ab8562b9e68bb333663d3098907bd2c95ab
Gaurav <SQX7@eurofins.com>
2023-12-07 16:59:11 +0530

    Version update for eu library

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'a16'`, () => {
+  it(`should have as title 'Application with eu-library'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('a16');
+    expect(app.title).toEqual('Application with eu-library');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('a16 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Application with eu-library app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Order, Position } from "eu-library";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'Playground';
+  title = "Playground";
+  public filterBy: string = "name";
+  public dragdrop: boolean = false;
+  public products: Order[] = [
+    {
+      image: "bamboo-watch.jpg",
+      name: "Bamboo Watch",
+      price: "65",
+      category: "Accessories",
+    },
+    {
+      image: "blue-band.jpg",
+      name: "Blue Band",
+      price: "79",
+      category: "Fitness",
+    },
+    {
+      image: "blue-t-shirt.jpg",
+      name: "Blue T-Shirt",
+      price: "29",
+      category: "Clothing",
+    },
+    {
+      image: "bracelet.jpg",
+      name: "Bracelet",
+      price: "15",
+      category: "Accessories",
+    },
+    {
+      image: "game-controller.jpg",
+      name: "Game Controller",
+      price: "99",
+      category: "Electronics",
+    },
+  ];
+
+  public sidebarVisible = false;
+  public position: Position = Position.Left;
+  public icon = 'pi-arrow-right';
 }
